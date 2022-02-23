82af34a7817f97dc7d9ce15651f9ff69913a14b4
Logos106 <logos106@outlook.com>
2022-02-23 18:11:34 +0900

    a

--- server/controller/salesOrder.controller.js
+++ server/controller/salesOrder.controller.js
@@ -241,17 +241,16 @@ async function update(req, res, next) {
           productPrice: unitPrice,
           productAmount: qty,
           productDeliveryOption: deliveryOption,
-          productRemark: remark,
           productType,
           ...restParams
         } = products[index];
+
         await salesOrder.addChairStock(stock, {
           through: {
             unitPrice,
             qty,
             deliveryOption,
             preOrder,
-            remark,
             ...restParams,
           },
         });
